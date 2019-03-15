b82aedd377e03db835b1f6c112df5e1d3ea207ba
Luis Hernandez <lhdez@iuma.ulpgc.es>
2019-03-15 12:43:08 +0000

    cambios menores

--- app/src/main/java/es/ulpgc/eite/cleancode/catalog/products/ProductListActivity.java
+++ app/src/main/java/es/ulpgc/eite/cleancode/catalog/products/ProductListActivity.java
@@ -34,7 +34,6 @@ public class ProductListActivity
     // Show the title in the action bar
     ActionBar actionBar = getSupportActionBar();
     if (actionBar != null) {
-      actionBar.setDisplayHomeAsUpEnabled(true);
       actionBar.setTitle(getString(R.string.title_product_list));
     }
 
